0c12e3d72f70f8f432f222e90a275e069b325733
Kamil <krusiec.kamil82@gmail.com>
2020-05-28 16:47:34 +0200

    Adding The Button Functionality To The Questions Activity

--- app/src/main/java/pl/krusiec/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/pl/krusiec/quizapp/QuizQuestionsActivity.kt
@@ -1,29 +1,78 @@
 package pl.krusiec.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var currentPosition: Int = 1
+    private var questionsList: ArrayList<Question>? = null
+    private var selectedOptionPosition: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-        val questionsList = Constants.getQuestions()
-        Log.i("Questions Size", "${questionsList.size}")
+        questionsList = Constants.getQuestions()
+        setQuestion()
 
-        val currentPosition = 1
-        val question: Question? = questionsList[currentPosition - 1]
+        tvOptionOne.setOnClickListener(this)
+        tvOptionTwo.setOnClickListener(this)
+        tvOptionThree.setOnClickListener(this)
+        tvOptionFour.setOnClickListener(this)
+    }
+
+    private fun setQuestion() {
+        currentPosition = 1
+        val question = questionsList!![currentPosition - 1]
+
+        defaultOptionsView()
 
         progressBar.progress = currentPosition
         tvProgress.text = "$currentPosition/${progressBar.max}"
-        tvQuestion.text = question!!.question
+        tvQuestion.text = question.question
         ivImage.setImageResource(question.image)
         tvOptionOne.text = question.optionOne
         tvOptionTwo.text = question.optionTwo
         tvOptionThree.text = question.optionThree
         tvOptionFour.text = question.optionFour
+    }
+
+    private fun defaultOptionsView() {
+        val options = ArrayList<TextView>()
+        options.add(0, tvOptionOne)
+        options.add(1, tvOptionTwo)
+        options.add(2, tvOptionThree)
+        options.add(3, tvOptionFour)
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this, R.drawable.default_option_border_bg)
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.tvOptionOne -> selectedOptionView(tvOptionOne, 1)
+            R.id.tvOptionTwo -> selectedOptionView(tvOptionTwo, 2)
+            R.id.tvOptionThree -> selectedOptionView(tvOptionThree, 3)
+            R.id.tvOptionFour -> selectedOptionView(tvOptionFour, 4)
+        }
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNumber: Int) {
+        defaultOptionsView()
+        selectedOptionPosition = selectedOptionNumber
+
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(this, R.drawable.selected_option_border_bg)
     }
 }
